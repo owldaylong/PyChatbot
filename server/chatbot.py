c231ed68bbdb8ff3b8cdda4fac4890f3d1ae6ac2
Anastasia Meicy <anastasiameicy@gmail.com>
2024-09-10 17:24:20 +0700

    Adding Flask to server & create base UI with Nextjs

---
+++ server/chatbot.py
@@ -0,0 +1,42 @@
+import os
+from flask import Flask, request, jsonify
+from groq import Groq
+
+app = Flask(__name__)
+
+api_key = os.getenv('GROQ_API_KEY')
+
+
+client = Groq(api_key=api_key)
+
+@app.route("/")
+def hello_world():
+    return "<p>Server is working wahooo!</p>"
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    data = request.json
+    user_input = data.get('input')
+
+    system_prompt = {
+        "role": "system",
+        "content":
+        "You are a helpful assistant with expertise in making social media post. Reply with at least 50 words in a paragraph and don't use hashtags. You're focused on aesthetic captions only."
+    }
+
+    chat_history = [system_prompt, {"role": "user", "content": user_input}]
+    # print(chat_history, "INI CHAT HISTORY")
+
+    response = client.chat.completions.create(
+        model="llama3-70b-8192",
+        messages=chat_history,
+        max_tokens=1000,
+        temperature=1.2
+    )
+    print(response, "INI RESPONSE")
+    assistant_reply = response.choices[0].message.content
+    return jsonify({"response": assistant_reply})
+    # return jsonify({"response": response})
+
+if __name__ == '__main__':
+    app.run(debug=True)
